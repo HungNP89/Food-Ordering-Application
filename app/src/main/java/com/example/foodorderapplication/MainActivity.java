8c9f9059fdc2bdcbae01c2f8a3bae2fc0e617798
HungNP89 <loliology9029@gmail.com>
2022-10-04 17:46:52 +0300

    Update layout for Welcome menu , login and register

--- app/src/main/java/com/example/foodorderapplication/MainActivity.java
+++ app/src/main/java/com/example/foodorderapplication/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.foodorderapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.bgImage);
+        textView = findViewById(R.id.textView);
+
+        imageView.animate().alpha(0f).setDuration(0);
+        textView.animate().alpha(0f).setDuration(0);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this,EntryMenu.class);
+            startActivity(intent);
+            finish();
+        },3000);
     }
 }
