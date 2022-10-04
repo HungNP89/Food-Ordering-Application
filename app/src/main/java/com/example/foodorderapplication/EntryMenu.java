088578d206306107dc33e0773d26e40d3fbacf13
HungNP89 <loliology9029@gmail.com>
2022-10-04 18:40:00 +0300

    Updated layout again

--- app/src/main/java/com/example/foodorderapplication/EntryMenu.java
+++ app/src/main/java/com/example/foodorderapplication/EntryMenu.java
@@ -9,17 +9,13 @@ import android.widget.TextView;
 
 public class EntryMenu extends AppCompatActivity {
     TextView textView2;
-    TextView textView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_menu);
 
         textView2 = findViewById(R.id.tvSignUp);
-        textView3 = findViewById(R.id.tvSignUp2);
-
         textView2.setOnClickListener(ClickSignUp());
-        textView3.setOnClickListener(ClickSignUp());
     }
 
     private View.OnClickListener ClickSignUp() {
